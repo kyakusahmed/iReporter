83be1ffc6686c76ec04581dad2f11a1d6f5020a9
kyakulumbye ahmad <kyakusahmed@outlook.com>
2018-12-17 17:11:05 +0300

    add environmental files

--- tests/test_views.py
+++ tests/test_views.py
@@ -38,7 +38,7 @@ class RedflagTest(unittest.TestCase):
             "body": "me and you",
             "location": "bundibugyo"
         }
-        response = self.app.post('/api/v1/redflags', json=data_test)
+        response = self.app2.post('/api/v1/redflags', json=data_test)
         data = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 201)
         assert data["message"] == "redflag added successfully"
